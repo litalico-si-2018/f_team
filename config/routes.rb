dd352b7837ebd39ea9e0b80d80fad1512e38a51b
wabisuke0724 <kanetsuki@komventure.net>
2018-08-11 20:13:23 +0900

    modefy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root   'minaos#main'
-  get 'contidion', to:'minaos#check_condition'
-  get 'result', to:'minaos#result'
+  get '/condition', to:'minaos#check_condition'
+  get '/result', to:'minaos#result'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
